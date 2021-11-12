522d1863e9103949b1e913715943d82ee0033874
Naveen240398 <naveenchandar626@gmail.com>
2021-11-12 10:33:04 +0530

    testing

---
+++ src/org/wipro/Enfield.java
@@ -0,0 +1,14 @@
+package org.wipro;
+
+public class Enfield {
+	
+	private void method1() {
+		System.out.println("I Am Naveen");
+	}
+
+	public static void main(String[] args) {
+		Enfield e = new Enfield();
+		e.method1();
+		
+	}
+}
